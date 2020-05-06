82fb0071dfb5df183f7b5302605dbb1bdd35d7f6
mrityagi <mrityagi@gmail.com>
2020-05-07 00:49:37 +0530

    added files

---
+++ precache-manifest.499d008265ba9097a300ef6d0d4d30e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24a908a1b397668e979a6ddd0f619d4b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "966d2539a32c089970fd",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "0da6c84932a63cbbea19",
+    "url": "/static/js/2.77945aaa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.77945aaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "966d2539a32c089970fd",
+    "url": "/static/js/main.a482ad6b.chunk.js"
+  },
+  {
+    "revision": "34a23f0ee4a1ee7d0d48",
+    "url": "/static/js/runtime-main.e5b1187f.js"
+  }
+]);
