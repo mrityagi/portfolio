da95314206ec83b06b213fc4c23bf7b607b45812
mrityagi <mrityagi@gmail.com>
2020-05-07 13:14:00 +0530

    changed a few things

---
+++ precache-manifest.2ea35f8f854a8f9bd750d422f1f39659.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0fae08a744b27059cafa91d6518eb15",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "157ad3f7911c54e5708c",
+    "url": "/portfolio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "0da6c84932a63cbbea19",
+    "url": "/portfolio/static/js/2.77945aaa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.77945aaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "157ad3f7911c54e5708c",
+    "url": "/portfolio/static/js/main.84f399b5.chunk.js"
+  },
+  {
+    "revision": "e345a16de91e7c06ddb7",
+    "url": "/portfolio/static/js/runtime-main.1879ff2c.js"
+  }
+]);
